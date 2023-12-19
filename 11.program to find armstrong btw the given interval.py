20275beb6c6fe0a02a0968be800f8c221c445de7
Vicky <126154136+vickyxiz@users.noreply.github.com>
2023-12-19 19:40:51 +0530

    Update 11.program to find armstrong btw the given interval.py

--- 11.program to find armstrong btw the given interval.py
+++ 11.program to find armstrong btw the given interval.py
@@ -6,6 +6,6 @@ for num in range(lower,upper + 1):
    while temp > 0:    
        digit = temp % 10    
        sum += digit ** 3    
-       temp //= 10    
+       temp //= 10   
        if num == sum:    
             print(num)    
