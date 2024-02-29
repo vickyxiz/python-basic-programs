2ff7b2eff6e7fc79154f7f1613e3b0745e4c9a0f
Vicky <126154136+vickyxiz@users.noreply.github.com>
2024-02-29 18:28:44 +0530

    Update 13.program to find the sum of  natural numbers.py

--- 13.program to find the sum of  natural numbers.py
+++ 13.program to find the sum of  natural numbers.py
@@ -2,7 +2,7 @@ a=int(input("enter a number:"))
 if(a<0):
     print("enter a postive number !!!")
 else:
-    sum_of=0
+    sum_of=0 
     for i in range(1,a+1):
         sum_of+=i
     print("The total sum is:",sum_of)
